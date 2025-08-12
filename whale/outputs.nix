fa5fe98f2efcab385e120b6b4ef235de57938c24
Yuto Nishida <yuto@berkeley.edu>
2025-08-11 21:53:45 -0700

    Whale: correct auth for skopeo

--- whale/outputs.nix
+++ whale/outputs.nix
@@ -9,37 +9,33 @@ in {
         contents = [ pkgs.curl ]; # Replace with your app
         config.Cmd = [ "curl" "--version" ];
       };
-    in {
-      whale-push-example =
-      pkgs.writeShellApplication {
-        name = "push-example";
+      make-push-script = { layeredImage, name }: pkgs.writeShellApplication {
+        name = "push-${name}";
+        # We have to use skopeo; docker CLI requires the docker daemon to be running.
         runtimeInputs = [ pkgs.skopeo pkgs.jq pkgs.docker-credential-helpers ];
         text = ''
           set -euo pipefail
 
-          dest="docker://docker.io/yuto7/example-image:latest"
+          dest="docker://docker.io/yuto7/${name}:latest"
 
           echo "Checking credentials..."
-          if [[ -n "''${REGISTRY_USER:-}" && -n "''${REGISTRY_PASS:-}" ]]; then
-            echo "Using creds from environment variables"
-            creds_opt=(--dest-creds "''${REGISTRY_USER}:''${REGISTRY_PASS}")
-          elif [[ -f "$HOME/.docker/config.json" ]]; then
-            echo "Using creds from ~/.docker/config.json"
-            creds_opt=()
+          if [[ -f "$HOME/.config/containers/auth.json" ]]; then
+            echo "Using creds from ~/.config/containers/auth.json"
           else
             echo "Error: No credentials found."
-            echo "Login with 'docker login registry.example.com' or set REGISTRY_USER and REGISTRY_PASS."
+            echo "Login with 'skopeo login docker.io'. You can use 'nix-shell -p skopeo' to get the skopeo command."
             exit 1
           fi
 
-          #echo "Building OCI image..."
-          #ociDir=$(mktemp -d)
-          #cp -r ${example-image} "$ociDir"
-
           echo "Pushing to $dest..."
-          skopeo copy "docker-archive:${example-image}" "$dest" "''${creds_opt[@]}"
+          skopeo copy "docker-archive:${layeredImage}" "$dest"
           echo "Done!"
         '';
+      };
+    in {
+      whale-push-example = make-push-script {
+        layeredImage = example-image;
+        name = "example-image";
       };
       whale-example-image = example-image;
     });
