d42f84e7d07c3b2b8a594737135840c3f9e3572e
Yuto Nishida <yuto@berkeley.edu>
2025-08-30 19:18:44 -0700

    Add Saturn

---
+++ flake-profiles/system-methane/flake.nix
@@ -0,0 +1,79 @@
+{
+  inputs = {
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+
+    soup = {
+      url = "github:starptr/soup";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-devenv.follows = "nixpkgs";
+        systems.follows = "systems";
+        devenv.follows = "devenv";
+      };
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, ... } @ inputs: let
+    forEachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
+    generated = builtins.fromJSON (builtins.readFile ./../../exports/jupiter/generated.json);
+  in {
+    devShells = forEachSystem (system: let
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in {
+      deploy = pkgs.mkShell {
+        buildInputs = [
+          pkgs.deploy-rs
+        ];
+      };
+      default = self.devShells.${system}.deploy;
+    });
+
+    nixosConfigurations.nixie = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        inputs.sops-nix.nixosModules.sops
+        {
+          nixpkgs = {
+            overlays = [
+              inputs.soup.overlays.chaseln
+            ];
+          };
+        }
+        ./../../venus/modules/nixos-darwin/methane.nix
+      ];
+    };
+
+    deploy.nodes.nixie = {
+      hostname = generated.nixie.ipAddress;
+      profilesOrder = [ "system" ];
+      profiles.system = {
+        user = "root";
+        sshUser = "root";
+        path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nixie;
+        remoteBuild = true;
+      };
+    };
+
+    # This is highly advised, and will prevent many possible mistakes
+    # Disabled to avoid remote builder
+    #checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) inputs.deploy-rs.lib;
+  };
+}
---
+++ venus/modules/nixos-darwin/methane.nix
@@ -0,0 +1,122 @@
+{ modulesPath, lib, pkgs, config, ... }:
+{
+  imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
+    (modulesPath + "/virtualisation/digital-ocean-config.nix")
+  ];
+
+  environment.systemPackages = [
+    pkgs.rclone
+    pkgs.vim
+    pkgs.chaseln
+    pkgs.htop
+  ];
+
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  system.stateVersion = "23.11"; # Do not change lightly!
+
+  # Config from the base image
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 3072;
+    }
+  ];
+  users.mutableUsers = false;
+  users.users.yuto = {
+    isNormalUser = true;
+    shell = pkgs.bash;
+    description = "Yuto";
+    password = "";
+    createHome = true;
+    homeMode = "755"; # Let other users read/search (eg. Komga) (The x bit for directories is actually for searching)
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLrT2/gQXhOz4E4xSphB8EXouild5qNOnZ6ZVXuTnf167z8xxSB10mxNey2gKDaIVig6I/tRFeYy6/N/QutbBlKI/+GNPjGCcVJI0hf7fTZGL4caTW8ggcXRz4LAsFp3JBf6Li0FVrGz5ojD0Etbl54BDn033q/tlVRhme5bXJ6s73yRg04kqdQsWVBRJwyzbUUmCQPrZd9i5Nh4QFVuhZljEyUWIStajE+c9v8OOiY1svv+XjKBjyWphP16HqgzvnEDf5+MQ5AUxE05IvJx43UY43CKTe3evzt4F/IqSdYwYGIQ55DaseRmf5zmHLU8MTTkksmOPQEzJL0nBzAmxyGV3PsMYPoIN+1/gJmxCO6ZaaCxYr9SFK/yoRW5e0PFX433xPhNsITBq7jUrVg6BQ/lr0ntRfvd7pRhFq8v02R3jWokL/99skxp1kjVF42bXEJXYPpHF3XAUhYscjOwmWj8dJgsIsSIKIjh7gRVYxQGrZQXOcJQjMytFgXy7fWHM= yuto@Yutos-MacBook-Pro.local" # Yuto's Sodium
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtVvX9uhSWD1DPBIRqgkNzFXqjdqvWB/WtDy4seaiJl" # 1Password "ssh key - main"
+    ];
+  };
+
+  # /var/lib/acme/.challenges must be writable by the ACME user
+  # and readable by the Nginx user. The easiest way to achieve
+  # this is to add the Nginx user to the ACME group.
+  users.users.nginx.extraGroups = [ "acme" ];
+
+  # Let's Encrypt
+  # see https://discourse.nixos.org/t/nixos-nginx-acme-ssl-certificates-for-multiple-domains/19608/3 for an example
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "acme.management@yart.me";
+      dnsProvider = "cloudflare";
+    };
+    certs."sp.nixie.yuto.sh" = {
+      dnsProvider = null;
+      webroot = config.services.nginx.virtualHosts."acmechallenge.yuto.sh".locations."/.well-known/acme-challenge".root;
+      # Ensure that the web server you use can read the generated certs
+      # Take a look at the group option for the web server you choose.
+      group = "nginx";
+      # Since we have a wildcard vhost to handle port 80,
+      # we can generate certs for anything!
+      # Just make sure your DNS resolves them.
+      #extraDomainNames = [ "mail.example.com" ];
+    };
+  };
+  security.sudo.wheelNeedsPassword = false;
+
+  # Nixie-specific config
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 80 443 ];
+    allowedUDPPorts = [ 443 ];
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts."hello-nginx.nixie.yuto.sh" = {
+      enableACME = true;
+      addSSL = true;
+      #locations."/".proxyPass = "http://127.0.0.1:9955/";
+      locations."/".extraConfig = ''
+        default_type text/html;
+        return 200 "<!DOCTYPE html><h1>Hello from hello-nginx!</h1>\n";
+      '';
+    };
+    #virtualHosts."komga.nixie.yuto.sh" = {
+    #  enableACME = true;
+    #  addSSL = true;
+    #  locations."/".extraConfig = ''
+    #    proxy_pass http://127.0.0.1:${builtins.toString config.services.komga.port};
+    #  '';
+    #};
+    virtualHosts."acmechallenge.yuto.sh" = {
+      # For acme HTTP challenge
+      locations."/.well-known/acme-challenge" = {
+        root = "/var/lib/acme/.challenges";
+      };
+    };
+  };
+
+  services.kubo = {
+    enable = false;
+  };
+
+  services.komga = {
+    enable = false;
+    #port = 8080;
+  };
+
+  services.syncplay = {
+    enable = false;
+    #port = 8999;
+    #passwordFile = ./syncplay-pw.txt;
+    #tls = config.security.acme.certs."sp.nixie.yuto.sh".directory;
+    #extraArgs = [ "--tls" (builtins.trace "Here I am ${config.security.acme.certs."sp.nixie.yuto.sh".directory}" config.security.acme.certs."sp.nixie.yuto.sh".directory) ];
+  };
+}
