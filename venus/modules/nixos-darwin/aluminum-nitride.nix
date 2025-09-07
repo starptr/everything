1399f1bd6ccc5835d0eb7a4110b1bf80d9fc5ca8
Yuto Nishida <yuto@berkeley.edu>
2025-09-07 00:28:44 -0700

    Migrate old evaluation options in aluminum-nitride

--- venus/modules/nixos-darwin/aluminum-nitride.nix
+++ venus/modules/nixos-darwin/aluminum-nitride.nix
@@ -46,8 +46,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -59,7 +59,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
