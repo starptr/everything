855cb13b394740eaa22646c929e2ecba4cfd2aca
Yuto Nishida <yuto@berkeley.edu>
2025-10-05 03:05:38 -0700

    Mvp e2e

--- experimental/chatproto/packages/client/src/pages/Channel.tsx
+++ experimental/chatproto/packages/client/src/pages/Channel.tsx
@@ -3,18 +3,46 @@ import StatusForm, { STATUS_OPTIONS } from '#/components/StatusForm'
 import StatusList from '#/components/StatusList'
 import { useAuth } from '#/hooks/useAuth'
 import { useParams, useSearchParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import api from '#/services/api'
+import { useEffect } from 'react'
 
 const Channel = () => {
   const { channelNsid } = useParams();
+  if (channelNsid === undefined) {
+    return <div>
+      <p>
+        Channel component requires channelNsid router param.
+      </p>
+    </div>;
+  }
   const [searchParams] = useSearchParams();
-  const channelOwnerHint = searchParams.get('hintChannelOwner');
+  const hintChannelOwner = searchParams.get('hintChannelOwner');
   const { user, loading, error } = useAuth()
+
+  const { data, isPending, isError, error: queryError } = useQuery({
+    queryKey: ['channel', channelNsid, hintChannelOwner],
+    queryFn: async () => {
+      const { data } = await api.getMessages({
+        channelNsid,
+        ...(hintChannelOwner !== null && { hintChannelOwner }), // Set field only if non-null
+      });
+      return data;
+    },
+    placeholderData: (previousData) => previousData, // Use previous data while refetching
+    refetchInterval: 30e3, // Refresh every second
+  });
+  useEffect(() => {
+    if (queryError) {
+      console.error(queryError);
+    }
+  }, [queryError]);
 
   // Get a random emoji from the STATUS_OPTIONS array
   const randomEmoji =
     STATUS_OPTIONS[Math.floor(Math.random() * STATUS_OPTIONS.length)]
 
-  if (loading) {
+  if (loading || isPending) {
     return (
       <div className="flex justify-center items-center h-[80vh]">
         <div className="text-9xl animate-pulse">{randomEmoji}</div>
@@ -22,7 +50,7 @@ const Channel = () => {
     )
   }
 
-  if (error) {
+  if (isError) {
     return (
       <div className="flex justify-center items-center py-16">
         <div className="text-center p-6 max-w-md">
@@ -41,6 +69,17 @@ const Channel = () => {
     )
   }
 
+  if (queryError) {
+    return (
+      <div>
+        <Header />
+        <p>
+          Query failed. Try refreshing?
+        </p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex flex-col gap-8 pb-12">
       <Header />
@@ -49,9 +88,14 @@ const Channel = () => {
 
       <div>
         <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200">
-          Recent Statuses
+          Messages
         </h2>
-        <StatusList />
+        {data.messages.map((messageData) => (
+          <div key={messageData.tid}>
+            <p>Author: {messageData.author.handle} ({messageData.author.did}) ({messageData.createdAt})</p>
+            <p>{messageData.plaintext ?? ""}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
