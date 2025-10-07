de4511022871ddc14d8340b07fd08b3cbcdd06a4
Yuto Nishida <yuto@berkeley.edu>
2025-10-07 00:05:37 -0700

    Add getMessages to appview api

--- experimental/chatproto/packages/appview/src/api/index.ts
+++ experimental/chatproto/packages/appview/src/api/index.ts
@@ -3,6 +3,7 @@ import { Server } from '#/lexicons'
 import getStatuses from './lexicons/getStatuses'
 import getUser from './lexicons/getUser'
 import sendStatus from './lexicons/sendStatus'
+import getMessages from './lexicons/getMessages'
 
 export * as health from './health'
 export * as oauth from './oauth'
@@ -11,5 +12,6 @@ export default function (server: Server, ctx: AppContext) {
   getStatuses(server, ctx)
   sendStatus(server, ctx)
   getUser(server, ctx)
+  getMessages(server, ctx)
   return server
 }
