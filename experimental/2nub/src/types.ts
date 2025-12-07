44c4ece9283493154b34a755b897a4facd0ae566
Yuto Nishida <yuto@berkeley.edu>
2025-12-06 17:11:38 -0800

    [2nub] Update types to handle game state defensively

--- experimental/2nub/src/types.ts
+++ experimental/2nub/src/types.ts
@@ -1,16 +1,74 @@
+const ROLES = [
+  //"copycat",
+  //"doppelganger",
+  "werewolf",
+  //"minion",
+  "seer",
+  "robber",
+  "troublemaker",
+  "villager",
+  "tanner",
+  "hunter",
+  "mason",
+  "insomniac",
+] as const;
+export type RoleId = typeof ROLES[number];
+
 export interface Player {
   id: string;
   name: string;
   connected: boolean;
 }
 
-export interface GameState {
-  id: string;
+export interface PlayerState {
+  readonly originalRoleId: RoleId;
+  currentRoleId: RoleId;
+  playerLog: string[];
+}
+
+export interface StateLobby {
+  state: 'lobby';
+};
+
+export interface StateNight {
+  state: 'night';
+  playerData: Record<Player["id"], PlayerState>;
+  centerCards: RoleId[];
+}
+
+export interface StateDay {
+  state: 'day';
+  playerData: StateNight["playerData"];
+  centerCards: StateNight["centerCards"];
+}
+
+export interface StateVoting {
+  state: 'voting';
+  votes: Record<Player["id"], Player["id"]>;
+  playerData: StateDay["playerData"];
+  centerCards: StateDay["centerCards"];
+}
+
+export interface StateFinished {
+  state: 'finished';
+  winnerRoleIds: RoleId[];
+  votes: StateVoting["votes"];
+  playerData: StateVoting["playerData"];
+  centerCards: StateVoting["centerCards"];
+}
+
+export type GameState = {
+  // Set on construction only
+  readonly id: string;
+  readonly createdAt: Date;
+
   name: string;
-  players: Player[];
-  status: 'waiting' | 'playing' | 'finished';
-  createdAt: Date;
   lastActivity: Date;
+  gameLog: string[];
+
+  players: Player[];
+  roleOrder: RoleId[];
+  state: StateLobby | StateNight | StateDay | StateVoting | StateFinished;
 }
 
 export interface CreateGameRequest {
