a1d564482db75a5f66fc049c81ad597465144458
Yuto Nishida <yuto@berkeley.edu>
2025-12-26 19:05:12 -0800

    [ATMii] Import bsky type defs

--- experimental/atmii/packages/client/src/main.tsx
+++ experimental/atmii/packages/client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type {} from '@atcute/bluesky'
 
 import App from 'client-src/App'
 
