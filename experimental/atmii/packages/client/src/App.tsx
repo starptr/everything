e7a7749b9bf0f53299cfa23eb03bbc7c6340a1de
Yuto Nishida <yuto@berkeley.edu>
2025-12-26 18:47:11 -0800

    [ATMii] Basic route examples

--- experimental/atmii/packages/client/src/App.tsx
+++ experimental/atmii/packages/client/src/App.tsx
@@ -5,6 +5,7 @@ import { AuthProvider } from 'client-src/hooks/useAuth'
 //import LoginPage from '#/pages/LoginPage'
 //import OAuthCallbackPage from '#/pages/OAuthCallbackPage'
 //import Channel from '#/pages/Channel'
+import Profile from 'client-src/pages/Profile'
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <div className="max-w-4xl mx-auto p-4 w-full">
         <AuthProvider>
           <Routes>
+            <Route path="/" element={<div>Home Page</div>} />
+            <Route path="/profile/:handle" element={<Profile />} />
             {/*
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
