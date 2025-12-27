bcffce017968e19688cf4a117ee2deb682bca1cd
Yuto Nishida <yuto@berkeley.edu>
2025-12-26 20:57:59 -0800

    [ATMii] Fetch mii record

--- experimental/atmii/packages/client/src/pages/Profile.tsx
+++ experimental/atmii/packages/client/src/pages/Profile.tsx
@@ -3,15 +3,45 @@ import { useParams, useSearchParams } from 'react-router'
 import { ComAtprotoRepoGetRecord } from '@atcute/atproto'
 import { Client, simpleFetchHandler } from '@atcute/client';
 import { isActorIdentifier } from '@atcute/lexicons/syntax';
+import {
+	CompositeHandleResolver,
+	DohJsonHandleResolver,
+	WellKnownHandleResolver,
+	CompositeDidDocumentResolver,
+	PlcDidDocumentResolver,
+	WebDidDocumentResolver,
+	LocalActorResolver,
+} from '@atcute/identity-resolver'
+import {
+	getPdsEndpoint
+} from '@atcute/identity'
 
 import { useState, useEffect, useMemo } from 'react'
 
+const handleResolver = new CompositeHandleResolver({
+	methods: {
+		dns: new DohJsonHandleResolver({ dohUrl: 'https://mozilla.cloudflare-dns.com/dns-query' }),
+		http: new WellKnownHandleResolver(),
+	},
+});
+
+const didResolver = new CompositeDidDocumentResolver({
+	methods: {
+		plc: new PlcDidDocumentResolver(),
+		web: new WebDidDocumentResolver(),
+	},
+});
+
+const actorResolver = new LocalActorResolver({
+	handleResolver,
+	didDocumentResolver: didResolver,
+});
+
 const Profile = () => {
 	const { handle } = useParams()
-
-	const rpc = useMemo(() => {
-		return new Client({ handler: simpleFetchHandler({ service: 'https://public.api.bsky.app' }), });
-	}, []);
+	const [pds, setPds] = useState<string | null>(null);
+	const [rpc, setRpc] = useState<Client>();
+	const [mii, setMii] = useState(new ArrayBuffer())
 
 	useEffect(() => {
 		if (!handle || !isActorIdentifier(handle)) return;
@@ -20,7 +50,14 @@ const Profile = () => {
 		console.log('Fetching profile for handle:', handle)
 
 		const fetchMii = async () => {
-			const result = rpc.get('com.atproto.repo.getRecord', {
+			// Fetch pds endpoint
+			const { pds } = await actorResolver.resolve(handle);
+			setPds(pds);
+
+			const rpc = new Client({ handler: simpleFetchHandler({ service: pds }), });
+			setRpc(rpc)
+
+			const result = await rpc.get('com.atproto.repo.getRecord', {
 				params: {
 					repo: handle,
 					collection: 'app.andref.atmii.mii',
