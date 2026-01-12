d0ae15eb6457480617c6559c7bdcf26a276e2f3c
Yuto Nishida <yuto@berkeley.edu>
2026-01-11 16:22:29 -0800

    [Chatproto3] Add actorResolver to MessageDemo

--- experimental/chatproto3/src/components/MessageDemo.tsx
+++ experimental/chatproto3/src/components/MessageDemo.tsx
@@ -1,4 +1,30 @@
 import React, { useState } from 'react';
+import {
+    CompositeHandleResolver,
+	DohJsonHandleResolver,
+	WellKnownHandleResolver,
+    CompositeDidDocumentResolver,
+    PlcDidDocumentResolver,
+	WebDidDocumentResolver,
+    LocalActorResolver,
+} from '@atcute/identity-resolver';
+const handleResolver = new CompositeHandleResolver({
+	strategy: 'race', // default - first successful response wins
+	methods: {
+        dns: new DohJsonHandleResolver({ dohUrl: 'https://mozilla.cloudflare-dns.com/dns-query' }),
+        http: new WellKnownHandleResolver(),
+    },
+});
+const didResolver = new CompositeDidDocumentResolver({
+	methods: {
+		plc: new PlcDidDocumentResolver(),
+		web: new WebDidDocumentResolver(),
+	},
+});
+const actorResolver = new LocalActorResolver({
+    handleResolver,
+    didDocumentResolver: didResolver,
+});
 
 interface MessageDemoProps {
     hintChannelOwner?: string;
