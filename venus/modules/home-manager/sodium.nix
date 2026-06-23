49176469c152c9e107334b068dfff80fccef867f
Yuto Nishida <yuto@berkeley.edu>
2026-06-23 00:08:10 -0700

    [sops] Add kubo proxy creds

--- venus/modules/home-manager/sodium.nix
+++ venus/modules/home-manager/sodium.nix
@@ -164,6 +164,12 @@
         sopsFile = ./../../../secrets/k8s-config/thelounge-gluetun.conf;
         path = "${config.magic.absolutePathStrings.sodium.milky-way-secrets}/thelounge-gluetun.conf";
       };
+      secrets."k8s-config/kubo-gluetun.conf" = {
+        format = "binary";
+        mode = "0400";
+        sopsFile = ./../../../secrets/k8s-config/kubo-gluetun.conf;
+        path = "${config.magic.absolutePathStrings.sodium.milky-way-secrets}/kubo-gluetun.conf";
+      };
       # Private key the grand-central reverse-tunnel LaunchAgent authenticates with (its public
       # half is grand-central's tunnelKeys in milky-way main.jsonnet). Binary so the PEM round-
       # trips byte-for-byte; 0600 because ssh rejects a group/world-readable identity file.
